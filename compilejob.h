199284d0d31d6866c7dd1e13c616e179c4ce667b
Drew Harris <harrisd@smu.edu>
2023-09-07 11:33:13 -0500

    Start compile job

---
+++ compilejob.h
@@ -0,0 +1,15 @@
+#include "job.h"
+
+class CompileJob : public Job {
+  public:
+    CompileJob(unsigned long jobChannels, int jobType)
+        : Job(jobChannels, jobType){};
+
+    ~CompileJob(){};
+
+    std::string output;
+    int returnCode;
+
+    void Execute();
+    void JobCompleteCallback();
+};
