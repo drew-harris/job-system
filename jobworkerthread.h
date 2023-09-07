49f186b982d2b90ec6576033d364e09def775c3d
Drew Harris <harrisd@smu.edu>
2023-09-07 12:07:03 -0500

    Add some member methods

--- jobworkerthread.h
+++ jobworkerthread.h
@@ -10,4 +10,14 @@ class JobWorkerThread {
     JobWorkerThread(const char *uniqueName, unsigned long workerJobChannels,
                     JobSystem *jobSystem);
     ~JobWorkerThread();
+
+    void Startup(); // Kickoff the thread which will call work()
+    void Work(); // Called in private thread, blocks forever until StopWorking()
+                 // is called
+
+    void Shutdown(); // Signal that work should stop at next opportunity
+
+    bool IsStopping() const;
+    void setWorkerJobChannels(unsigned long workerJobChannels);
+    static void WokerThreadMain(void *workThreadObject);
 };
