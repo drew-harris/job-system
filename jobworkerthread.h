a7ef3170fe73a1f35e81d20cf731eeec3012b06b
Drew Harris <harrisd@smu.edu>
2023-09-07 12:09:50 -0500

    Add private members

--- jobworkerthread.h
+++ jobworkerthread.h
@@ -20,4 +20,13 @@ class JobWorkerThread {
     bool IsStopping() const;
     void setWorkerJobChannels(unsigned long workerJobChannels);
     static void WokerThreadMain(void *workThreadObject);
+
+  private:
+    const char *m_uniqueName;
+    unsigned long m_workerJobChannels = 0xffffffff;
+    bool m_isStopping = false;
+    JobSystem *m_jobSystem = nullptr;
+    std::thread *m_thread = nullptr;
+
+    mutable std::mutex m_workerStatusMutex;
 };
