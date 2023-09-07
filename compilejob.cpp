aae7a7aa6814cc83f1a9e75c70737b2665c66d6b
Drew Harris <harrisd@smu.edu>
2023-09-07 11:44:11 -0500

    Add comments

--- compilejob.cpp
+++ compilejob.cpp
@@ -10,16 +10,18 @@ void CompileJob::Execute() {
     // Redirect cerr to cout
     command.append(" 2>&1");
 
+    // open pipe and run command
     FILE *pipe = popen(command.c_str(), "r");
-
     if(!pipe) {
         std::cout << "popen Failed: Error: failed to open pipe" << std::endl;
         return;
     }
 
+    // read till end of process:
     while(fgets(buffer.data(), 128, pipe) != nullptr) {
         this->output.append(buffer.data());
     }
 
+    // close pipe and get return code
     this->returnCode = pclose(pipe);
 }
